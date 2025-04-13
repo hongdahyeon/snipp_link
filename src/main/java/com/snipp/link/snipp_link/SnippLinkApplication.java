3d02b02ca4bfdc277cf0c333d1736a06c7f64c36
hongdahyeon <hyeon8287@gmail.com>
2025-04-13 22:19:59 +0900

    최초 커밋

---
+++ src/main/java/com/snipp/link/snipp_link/SnippLinkApplication.java
@@ -0,0 +1,13 @@
+package com.snipp.link.snipp_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippLinkApplication.class, args);
+	}
+
+}
